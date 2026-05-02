8515d04155c80b83f56513e0eb889720aae8fb6b
thetnaing <thetnaingoo1530@gmail.com>
2026-05-02 15:35:21 +0630

    done testing zustand stores topic & implement unit test for counterStore

---
+++ src/counterStore.js
@@ -0,0 +1,17 @@
+// counterStore.js
+
+import {create} from 'zustand'
+
+const useCounterStore = create(set => ({
+	counter : 0,
+	actions : {
+		increment : ()=> set(state => ({counter : state.counter + 1})),
+		decrement : ()=> set(state => ({counter : state.counter - 1})),
+		zero : ()=> set(state => ({counter : 0})),
+	}
+}))
+
+export const useCounter = ()=> useCounterStore(state => state.counter)
+export const useCounterControls = ()=> useCounterStore(state => state.actions)
+
+export default useCounterStore
